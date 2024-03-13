c780a1cb5a19830ba3d88f39439aa73c77a789f7
Emmanuel <emmanuelcibe@yahoo.com>
2024-03-13 04:01:19 +0100

    created app

---
+++ best_one-app/app.py
@@ -0,0 +1,24 @@
+from flask import Flask, render_template, requests, redirect, url_for, jasonify
+
+import psycopg2
+import keyring
+
+app = Flask(__name__, static_url_path='/static')
+app.config['SECRET_KEY'] = 'best_one'
+
+#lets confgure the database 
+
+db_host = "127.0.0.1"
+db_name = "best_one"
+db_user = "postgres"
+db_password = keyring.get_password('best_one', 'postgres')
+
+#lets create a route for homepage
+
+@app.route('/')
+def homepage():
+    return render_template('index.html')
+
+if __name__ == '__main__':
+    app.run(debug=True)
+
